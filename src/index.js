13df4fc73b488a50fe27624c05a38a35ae044941
arifpehlivan <arifpehlivanapap@gmail.com>
2022-04-08 04:03:07 +0300

    state hook

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
 import { createRoot } from 'react-dom/client';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
 
-class App extends Component {
-  render() {
-    return (
-      <div>index</div>
-    )
-  }
+// class App extends Component {
+//   constructor(props){
+//     super(props);
+//     this.state={
+//       count: 0
+//     }
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <p>Clicked {this.state.count}</p>
+//         <button onClick={()=> this.setState({count: this.state.count +1})}>+1</button>
+//       </div>
+//     )
+//   }
+// }
+
+const App = (props) => {
+  const [count, setCount] = useState(props.count);
+  const [text, setText] = useState("");
+  return (
+    <div>
+      <p>Clicked {count}</p>
+      <p>Text: {text}</p>
+      <button onClick={()=> setCount(count+1)}>+1</button>
+      <button onClick={()=> setCount(count-1)}>-1</button>
+      <button onClick={()=> setCount(props.count)}>reset</button>
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+    </div>
+  )
+}
+
+App.defaultProps = {
+  count: 0
 }
 
 root.render(<App/>);
